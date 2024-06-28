def11462eab262e08d9145fa48f8092840b5a402
aravind8204 <aravindhanvgc@gmail.com>
2024-06-28 21:47:49 +0530

    initialization

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <>
+    <header>Login</header>
+    <content>
+        <div>
+            <div>
+                <></>
+            </div>
+            <div>
+                <></>
+            </div>
+        </div>
+    </content>
+    </>
+  )
+}
